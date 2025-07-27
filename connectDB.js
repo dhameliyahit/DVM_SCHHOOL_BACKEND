8becbaddb0021fd1fc1ca97d1ae04d5a4940fa97
dhameliyahit <heetdhameliya59@gmail.com>
2025-07-28 00:31:51 +0530

    loacal db url added

--- connectDB.js
+++ connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async (DB_URL) => {
     try {
-        mongoose.connect(DB_URL)
+        mongoose.connect(DB_URL || "mongodb://localhost:27017/Sarasvati-school")
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Error connecting to the database:', error);
